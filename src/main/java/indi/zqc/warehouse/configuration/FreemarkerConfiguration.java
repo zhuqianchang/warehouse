77b0e0ee315ddf64a90a40112b9d25d9dfce65ea
Zhu.Qianchang <Zhu.Qianchang@geely.com>
2018-03-29 13:09:57 +0800

    Freemarker BUG修复

--- src/main/java/indi/zqc/warehouse/configuration/FreemarkerConfiguration.java
+++ src/main/java/indi/zqc/warehouse/configuration/FreemarkerConfiguration.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Title : FreemarkerConfiguration.java
@@ -35,10 +34,23 @@ public class FreemarkerConfiguration {
         FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
         configurer.setTemplateLoaderPaths(properties.getTemplateLoaderPath());
         configurer.setDefaultEncoding("UTF-8");
+        configurer.setFreemarkerSettings(freemarkerSettings());
         Map<String, Object> variables = new HashMap<>();
         variables.put("buttonRight", buttonDirective());
         configurer.setFreemarkerVariables(variables);
         return configurer;
+    }
+
+    public Properties freemarkerSettings() {
+        Properties freemarkerSettings = new Properties();
+        Map<String, String> settings = properties.getSettings();
+        Set<Map.Entry<String, String>> entries = settings.entrySet();
+        Iterator<Map.Entry<String, String>> it = entries.iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> entry = it.next();
+            freemarkerSettings.setProperty(entry.getKey(), entry.getValue());
+        }
+        return freemarkerSettings;
     }
 
 }
